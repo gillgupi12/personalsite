c26d19ac8b6bdc1bafacfd1c9b74bbe1fad523b7
gurpindersinghgill <gupigill12@gmail.com>
2024-07-27 16:45:45 +0800

    feat: remove contact view

--- personalsite/src/views/ContactView.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <section class="flex flex-col gap-4 text-slate-300  py-24">
-    <p class="text-slate-400">Built using Vue3, inspired by brittanychiang!</p>
-  </section>
-</template>
